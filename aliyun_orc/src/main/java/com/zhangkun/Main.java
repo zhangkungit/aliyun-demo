68ac50c514d3c77de1402f9438ea7df5a4cceef9
zhangkun <zhangkun@yryz.com>
2017-07-20 14:15:01 +0800

    config logback

---
+++ aliyun_orc/src/main/java/com/zhangkun/Main.java
@@ -0,0 +1,17 @@
+package com.zhangkun;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author zhangkun
+ * @version 1.0
+ * @date 2017/7/20
+ * @description
+ */
+public class Main {
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+    public static void main(String[] args) {
+        logger.info("my name is:{}", "zhangkun");
+    }
+}
